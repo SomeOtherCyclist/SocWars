b405129616e9ebcd868567d92c7a458bc74b1c16
LiamShelley <66814870+SomeOtherCyclist@users.noreply.github.com>
2025-07-21 18:24:25 +1000

    Initial commit

---
+++ src/client/java/com/soc/SocWarsClient.java
@@ -0,0 +1,10 @@
+package com.soc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SocWarsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
